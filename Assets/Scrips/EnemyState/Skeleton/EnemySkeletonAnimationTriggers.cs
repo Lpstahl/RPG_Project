5db9e7641ab02e9f8d46e7a9c8d3bf2920c94e19
Lpstahl <lpstahl.dev@gmail.com>
2023-04-25 12:49:42 -0300

    Update EnemySkeletonAnimationTriggers.cs

--- Assets/Scrips/EnemyState/Skeleton/EnemySkeletonAnimationTriggers.cs
+++ Assets/Scrips/EnemyState/Skeleton/EnemySkeletonAnimationTriggers.cs
@@ -10,4 +10,17 @@ public class EnemySkeletonAnimationTriggers : MonoBehaviour
     {
         enemy.AnimationFinishTrigger();
     }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(enemy.attackCheck.position, enemy.attackCheckRadius);
+
+        foreach(var hit in colliders)
+        {
+            if (hit.GetComponent<Player>() != null)
+            {
+                hit.GetComponent<Player>().Damage();
+            }
+        }
+    }
 }
