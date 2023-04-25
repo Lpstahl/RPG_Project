f0c8bf5215dd2445e6e846eb140d31f8ca81fa96
Lpstahl <lpstahl.dev@gmail.com>
2023-04-25 12:49:49 -0300

    Update PlayerAnimationTrigger.cs

--- Assets/Scrips/Player State/PlayerAnimationTrigger.cs
+++ Assets/Scrips/Player State/PlayerAnimationTrigger.cs
@@ -10,4 +10,17 @@ public class PlayerAnimationTrigger : MonoBehaviour
     {
         player.AnimationTrigger();
     }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(player.attackCheck.position, player.attackCheckRadius);
+
+        foreach(var hit in colliders)
+        {
+            if(hit.GetComponent<Enemy>() != null)
+            {
+                hit.GetComponent<Enemy>().Damage();
+            }
+        }
+    }
 }
