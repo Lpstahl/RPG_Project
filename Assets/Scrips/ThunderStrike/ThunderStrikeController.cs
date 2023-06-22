a5ba4cdabc3357815c4df3630438a54c90851ddf
Lpstahl <lpstahl.dev@gmail.com>
2023-06-22 10:52:42 -0300

    thunderstrike update

--- Assets/Scrips/ThunderStrike/ThunderStrikeController.cs
+++ Assets/Scrips/ThunderStrike/ThunderStrikeController.cs
@@ -50,7 +50,7 @@ public class ThunderStrikeController : MonoBehaviour
     private void DamageAndSelfDestroy()
     {
         targetStats.ApplyShock(true);  
-        targetStats.TakeDamage(1);
+        targetStats.TakeDamage(damage);
         Destroy(gameObject, .4f);
     }
 }
